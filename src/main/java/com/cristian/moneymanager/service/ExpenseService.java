b13b6ce288981240717b7d795a7648cd78691327
CriFigShe <crifigshe@gmail.com>
2025-09-23 15:17:12 +0200

    Expense and income service

---
+++ src/main/java/com/cristian/moneymanager/service/ExpenseService.java
@@ -0,0 +1,57 @@
+package com.cristian.moneymanager.service;
+
+import com.cristian.moneymanager.dto.ExpenseDto;
+import com.cristian.moneymanager.entity.CategoryEntity;
+import com.cristian.moneymanager.entity.ExpenseEntity;
+import com.cristian.moneymanager.entity.ProfileEntity;
+import com.cristian.moneymanager.repository.CategoryRepository;
+import com.cristian.moneymanager.repository.ExpenseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseService {
+
+    private final CategoryRepository categoryRepository;
+    private final ExpenseRepository expenseRepository;
+    private final ProfileService profileService;
+
+    public ExpenseDto addExpense(ExpenseDto dto) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        CategoryEntity category = categoryRepository.findById(dto.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        ExpenseEntity newExpense = toEntity(dto, profile, category);
+        expenseRepository.save(newExpense);
+
+        return toDto(newExpense);
+    }
+
+    private ExpenseEntity toEntity(ExpenseDto dto, ProfileEntity profile, CategoryEntity category) {
+        return ExpenseEntity.builder()
+                .name(dto.getName())
+                .icon(dto.getIcon())
+                .amount(dto.getAmount())
+                .date(dto.getDate())
+                .profile(profile)
+                .category(category)
+                .build();
+    }
+
+    private ExpenseDto toDto(ExpenseEntity entity) {
+        return ExpenseDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .icon(entity.getIcon())
+                .categoryId(entity.getCategory() != null ? entity.getCategory().getId() : null)
+                .categoryName(entity.getCategory() != null ? entity.getCategory().getName() : "N/A")
+                .amount(entity.getAmount())
+                .date(entity.getDate())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
+}
---
+++ src/main/java/com/cristian/moneymanager/service/IncomeService.java
@@ -0,0 +1,55 @@
+package com.cristian.moneymanager.service;
+
+import com.cristian.moneymanager.dto.IncomeDto;
+import com.cristian.moneymanager.entity.CategoryEntity;
+import com.cristian.moneymanager.entity.IncomeEntity;
+import com.cristian.moneymanager.entity.ProfileEntity;
+import com.cristian.moneymanager.repository.CategoryRepository;
+import com.cristian.moneymanager.repository.IncomeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class IncomeService {
+
+    private final CategoryRepository categoryRepository;
+    private final IncomeRepository incomeRepository;
+    private final ProfileService profileService;
+
+    public IncomeDto addIncome(IncomeDto dto) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        CategoryEntity category = categoryRepository.findById(dto.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        IncomeEntity newIncome = toEntity(dto, profile, category);
+        incomeRepository.save(newIncome);
+
+        return toDto(newIncome);
+    }
+
+    private IncomeEntity toEntity(IncomeDto dto, ProfileEntity profile, CategoryEntity category) {
+        return IncomeEntity.builder()
+                .name(dto.getName())
+                .icon(dto.getIcon())
+                .amount(dto.getAmount())
+                .date(dto.getDate())
+                .profile(profile)
+                .category(category)
+                .build();
+    }
+
+    private IncomeDto toDto(IncomeEntity entity) {
+        return IncomeDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .icon(entity.getIcon())
+                .categoryId(entity.getCategory() != null ? entity.getCategory().getId() : null)
+                .categoryName(entity.getCategory() != null ? entity.getCategory().getName() : "N/A")
+                .amount(entity.getAmount())
+                .date(entity.getDate())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
+}
